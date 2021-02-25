42e91f41e61dd48a7b6f6ce8150ebb0a47f16555
Thanh Chiến <70896118+jackychun2002@users.noreply.github.com>
2021-02-25 22:52:29 +0700

    assiment3 a

---
+++ src/asiment3/Product.java
@@ -0,0 +1,57 @@
+package asiment3;
+
+import java.math.BigDecimal;
+
+public class Product {
+    private int id;
+    private String name;
+    private int qty;
+    private BigDecimal price;
+
+    public Product(){
+    }
+
+    public Product(int id,String name,int qty,BigDecimal price){
+        this.id=id;
+        this.name= name;
+        this.qty=qty;
+        this.price=price;
+    }
+    public int getId(){
+        return id;
+    }
+
+    public void setId(int id){
+        this.id=id;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public int getQty(){
+        return qty;
+    }
+
+    public void setQty(int qty){
+        if(qty<0){
+            System.out.println("so luong san pham lon hon 0");
+            return;
+        }
+        this.qty=qty;
+    }
+    public BigDecimal getPrice(){
+        return price;
+    }
+    public void setPrice(BigDecimal price){
+        if(price.compareTo(BigDecimal.ZERO)== -1){
+            System.out.println("so luong san pham lon 0");
+            return;
+        }
+        this.price=price;
+    }
+}
