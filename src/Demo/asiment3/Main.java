876e20097b48a46380515af03f3ba9a7fe20050d
Thanh Chiến <70896118+jackychun2002@users.noreply.github.com>
2021-02-26 11:19:12 +0700

    assiment3

---
+++ src/Demo/asiment3/Main.java
@@ -0,0 +1,35 @@
+package Demo.asiment3;
+
+public class Main {
+    public static void main(String[] args){
+        Product p1 = new Product();
+        p1.setId(1);
+        p1.setPrice(15000);
+        p1.setProductName("Dong ho Rolex");
+        p1.setQty(2);
+        Product p2 = new Product();
+        p2.setId(2);
+        p2.setPrice(3000);
+        p2.setProductName("Vay thoi trang");
+        p2.setQty(5);
+
+        Cart c = new Cart();
+        c.setId(1);
+        c.setCustomer("Nguyen Van A");
+        c.setCity("hai duong");
+        c.addProduct(p1);
+        c.addProduct(p1);
+        c.addProduct(p1);
+        c.addProduct(p2);
+        c.addProduct(p2);
+        c.addProduct(p2);
+        // 15000 *2 + 3000*3 = 39000 -> 39000 + 390 = 39390
+
+
+        System.out.println("Danh sach san pham:");
+        for (Product p : c.getProductList()){
+            System.out.println(p.getId()+"- "+p.getProductName()+"-"+p.getPrice());
+        }
+        System.out.println("Tong tien:"+c.finalGrandTotal());
+    }
+}
---
+++ src/Demo/asiment3/Product.java
@@ -0,0 +1,52 @@
+package Demo.asiment3;
+
+public class Product { int id;
+    String productName;
+    int qty;
+    double price;
+
+    public Product() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public int getQty() {
+        return qty;
+    }
+
+    public void setQty(int qty) {
+        this.qty = qty;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public boolean checkQty(){
+        if(qty>0){
+            System.out.println("Con hang");
+            return true;
+        }
+        System.out.println("Het hang");
+        return false;
+
+    }
+}
